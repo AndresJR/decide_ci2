2b0894f8e732ddc3a1ad27b1ca5045a8dd6229ee
Andres <ajramirez@us.es>
2020-12-17 16:22:38 +0000

    Revert "Version inicial para despliegue en travis y heroku"
    This reverts commit 2e32a68f7a4f758689ed1dbb1633a0a48ec9e44e.

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -165,8 +165,6 @@ KEYBITS = 256
 ALLOWED_VERSIONS = ['v1', 'v2']
 DEFAULT_VERSION = 'v1'
 
-BASEURL= "https://egc-app-decide-pro.herokuapp.com"
-
 try:
     from local_settings import *
 except ImportError:
@@ -182,6 +180,3 @@ if os.path.exists("config.jsonnet"):
 
 
 INSTALLED_APPS = INSTALLED_APPS + MODULES
-
-import django_heroku
-django_heroku.settings(locals())
